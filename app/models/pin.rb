09d9625518b0858161e965f108a9732647aecba8
Seba Gamboa <me@sagmor.com>
2014-04-09 17:01:23 -0500

    Pin validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,4 @@
 class Pin < ActiveRecord::Base
+  validates :title, presence: true
+  validates :image, presence: true
 end
--- test/models/pin_test.rb
+++ test/models/pin_test.rb
@@ -1,4 +1,32 @@
 require 'test_helper'
 
 class PinTest < ActiveSupport::TestCase
+
+  test "tiene que tener un nombre" do
+    @pin = Pin.new({
+      title: '',
+      image: 'http://example.com/imagen.png'
+    })
+
+    assert !@pin.valid?, "Un pin sin nombre no debe ser valido!"
+  end
+
+  test "tiene que tener una imagen" do
+    @pin = Pin.new({
+      title: 'Un pin increible',
+      image: ''
+    })
+
+    assert !@pin.valid?, "Un pin sin imagen no debe ser valido!"
+  end
+
+  test "tiene que validar" do
+    @pin = Pin.new({
+      title: 'Un pin increible',
+      image: 'http://example.com/imagen.png'
+    })
+
+    assert @pin.valid?, "El pin debería ser valido"
+  end
+
 end
