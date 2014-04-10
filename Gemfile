17bb37ba7ca2a6cc853520d1d1f49b3d579716cf
Seba Gamboa <me@sagmor.com>
2014-04-10 10:49:33 -0500

    Add basic authentication

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'spring',        group: :development
 
 gem 'carrierwave'
 gem 'bootstrap-sass'
+gem 'devise'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /pins
   # GET /pins.json
